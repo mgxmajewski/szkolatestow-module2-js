71914dd89daa450d7e20e8c798f8b37844eb1932
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-03 01:07:25 +0200

    Wrap tests in describe block

--- homework2_1of2.test.js
+++ homework2_1of2.test.js
@@ -1,26 +1,30 @@
 const {VatService, Product} = require('./homework2_1of2')
 
- // beforeEach(() => {
- //     // Prepares vatService
- //
- //     const vat = new VatService
- //     return vat
- // });
-let vat = new VatService
+describe('VatService', () => {
+    let vatService
+    beforeEach(() => {
+        console.log('before each')
+        vatService = new VatService()
+    });
 
-it('test_get_gross_price_for_default_vat', () => {
-    let product = new Product(1, 20.00)
-    let result = vat.getGrossPriceForDefaultVat(product)
-    expect(result).toEqual(24.60);
-});
+    let getProductWithPrice = (id, netPrice) => {
+        return new Product(id, netPrice)
+    }
 
-it('test_get_gross_price', () => {
-    let product = new Product(1, 10.00)
-    const vatValue = 0.08
-    let result = vat.getGrossPrice(product.getNetPrice(), vatValue)
-    console.log(result)
-    expect(result).toEqual(10.80);
-});
+    test('test_get_gross_price_for_default_vat', () => {
+        let product = getProductWithPrice(1, 20.00)
+        let result = vatService.getGrossPriceForDefaultVat(product)
+        expect(result).toEqual(24.60);
+    });
+
+    test('test_get_gross_price', () => {
+        let product = getProductWithPrice(1, 10.00)
+        const vatValue = 0.08
+        let result = vatService.getGrossPrice(product.getNetPrice(), vatValue)
+        expect(result).toEqual(10.80);
+    });
+})
+
 
 
 
