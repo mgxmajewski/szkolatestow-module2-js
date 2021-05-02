bace2c553479c75c0c603522e44eaee78dabf8b6
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-02 02:57:01 +0200

    Add test of getGrossPrice

--- homework2_1of2.test.js
+++ homework2_1of2.test.js
@@ -14,6 +14,14 @@ it('test_get_gross_price_for_default_vat', () => {
     expect(result).toEqual(24.60);
 });
 
+it('test_get_gross_price', () => {
+    let product = new Product(1, 10.00)
+    const vatValue = 0.08
+    let result = vat.getGrossPrice(product.getNetPrice(), vatValue)
+    console.log(result)
+    expect(result).toEqual(10.80);
+});
+
 
 
 
