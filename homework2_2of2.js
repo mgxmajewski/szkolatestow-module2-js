40862ba8e72356bf91bd5716477ad06b0a6dcbd1
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-04 01:23:39 +0200

    Add calculateGrossPrice for calculating vat

--- homework2_2of2.js
+++ homework2_2of2.js
@@ -2,15 +2,22 @@ const {VatProvider} = require('./VatProvider')
 
 class VatService{
     constructor(VatProvider) {
-        this.VatProvider = VatProvider.getDefaultVat()
+        this.VatProvider = VatProvider
     }
 
     getGrossPriceForDefaultVat(product){
-        return this.getGrossPrice(product.getNetPrice(), this.VatProvider);
+        return this.calculateGrossPrice(product.getNetPrice(), this.VatProvider.getDefaultVat());
     }
 
-    getGrossPrice(netPrice, vatValue){
+    getGrossPriceForType(netPrice, productType){
+        const vatValue = this.VatProvider.getVatForType(productType)
+        console.log('netPrice' + netPrice)
+        return this.calculateGrossPrice(netPrice, vatValue)
+    }
+
+    calculateGrossPrice(netPrice, vatValue){
         let result
+        console.log(vatValue)
         if(vatValue > 1){
             throw new Error('valueError - exceed one')
         } else if (typeof vatValue == "string"){
@@ -18,8 +25,8 @@ class VatService{
         } else if (vatValue < 0) {
             throw new Error('valueError - negative')
         }
-        result = netPrice * (1 + vatValue)
-        result.toPrecision(5)
+        result = parseFloat((netPrice * (1 + vatValue)).toPrecision(5))
+        console.log(result)
         return result
     }
 }
@@ -39,6 +46,11 @@ class Product {
         return this.productType
     }
 }
+
+let product = new Product(1, 10.00, 'burger')
+let vatService = new VatService(VatProvider)
+const vatValue = 0.08
+console.log(vatService.calculateGrossPrice(product.getNetPrice(), vatValue))
 
 module.exports = {VatService, Product}
 
