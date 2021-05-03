ddedd13908d71cfdcf39f832ca64edc1852183e2
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-03 20:25:44 +0200

    Add tests for setter and getter for default vat

--- homework2_1of2.test.js
+++ homework2_1of2.test.js
@@ -1,9 +1,8 @@
-const {VatService, Product} = require('./homework2_1of2')
+const {VatService, Product} = require('./homework2_2of2')
 
 describe('VatService', () => {
     let vatService
     beforeEach(() => {
-        console.log('before each')
         vatService = new VatService()
     });
 
@@ -15,6 +14,17 @@ describe('VatService', () => {
         let product = getProductWithPrice(1, 20.00)
         let result = vatService.getGrossPriceForDefaultVat(product)
         expect(result).toEqual(24.60);
+    });
+
+    test('test getter for default vat', () => {
+        let result = vatService.getDefaultVatValue
+        expect(result).toEqual(0.23);
+    });
+
+    test('test setter for default vat', () => {
+        vatService.setDefaultVatValue = 0.25
+        let result = vatService.getDefaultVatValue
+        expect(result).toEqual(0.25);
     });
 
     test('test_get_gross_price', () => {
@@ -30,7 +40,7 @@ describe('VatService', () => {
         function result() {
             vatService.getGrossPrice(product.getNetPrice(), vatValue)
         }
-        expect(() => result()).toThrow('valueError');
+        expect(() => result()).toThrow('valueError - exceed one');
     });
 })
 
