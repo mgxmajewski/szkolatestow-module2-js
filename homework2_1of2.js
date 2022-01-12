5a3bcd06b7708a0bee52e61e5b89270554720243
mgxmajewski <mgxmajewski@gmail.com>
2022-01-12 21:24:52 +0100

    Update homework2_1of2.js

--- homework2_1of2.js
+++ homework2_1of2.js
@@ -1,29 +1,3 @@
-// class VatService {
-//     double vatValue;
-//
-//     public VatService() {
-//         this.vatValue = 0.23;
-//     }
-//
-//     public double getGrossPriceForDefaultVat(Product product){
-//
-//     return getGrossPrice(product.getNetPrice(), vatValue);
-// }
-//
-// public double getGrossPrice(double netPrice, double vatValue){
-//     if(vatValue > 1){
-//         throw Exception
-//     }
-//
-//     return netPrice * (1 + vatValue)));
-// }
-// }
-//
-// class Product {
-//     string id;
-//     double netPrice;
-// }
-
 class VatService{
     constructor(vatValue=0.23) {
         this.vatValue = vatValue
